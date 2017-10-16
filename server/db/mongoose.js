3ca1abfe1b7a86ecfc8eb0a974f6efc07f1dffed
khlokesh-184 <khlokesh184@gmail.com>
2017-10-16 20:48:57 +0530

    Add POST /todos

---
+++ server/db/mongoose.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+//configure mongoose
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+module.exports={
+	mongoose: mongoose
+};
---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var {mongoose}=require('./db/mongoose');
+var{Todo}=require('./models/todos');
+var{User}=require('./models/user');
+
+var app=express();
+
+//bpdy-parser is a middleware for express
+//bodyParser.json() returns a function which express can use
+app.use(bodyParser.json());
+
+//"/todos" is used to create a new todo
+app.post('/todos', (req,res)=> {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc);
+	},(e)=>{
+		res.status(400).send(e);
+	})
+});
+
+app.listen(3000, () => {
+	console.log('Started on port 3000');
+});
