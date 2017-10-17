a79af3bf36785e943d06743f99034ea4684dd740
khlokesh-184 <khlokesh184@gmail.com>
2017-10-17 23:49:32 +0530

    heroku settings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //configure mongoose
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://khlokesh-184:khlokesh184@ds121945.mlab.com:21945/my-todos' || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports={
