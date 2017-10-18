4c90a10ab376433fba653284058aacb38be2ccd1
khlokesh-184 <khlokesh184@gmail.com>
2017-10-18 21:02:28 +0530

    nxjj

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //configure mongoose
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://khlokesh:khlokesh184@ds121665.mlab.com:21665/todo-app');
+mongoose.connect('mongodb://khlokesh:khlokesh184@ds121665.mlab.com:21665/todo-app' || 'mongodb://localhost:27017/TodoApp');
 
 //|| 
 
